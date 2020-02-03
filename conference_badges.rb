ac659df87e544091fbe75e61debd9cb41389bf56
Luis Alberto Servin <lservin2015@gmail.com>
2020-02-03 14:53:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,7 +9,7 @@ end
 def batch_badge_creator(array)
   newarray = []
   array.each do |name|
-    newarray.push("Hello, my name is #{attendee}.")
+    newarray.push("Hello, my name is #{name}.")
   end 
   return newarray
 end
